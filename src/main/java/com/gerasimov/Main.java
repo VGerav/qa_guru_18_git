1167337f6217fd944ea01e4e79a2c536884827ec
VGerav <92693397+VGerav@users.noreply.github.com>
2024-01-14 14:14:23 +0300

    Update Main.java

--- src/main/java/com/gerasimov/Main.java
+++ src/main/java/com/gerasimov/Main.java
@@ -2,7 +2,7 @@ package com.gerasimov;
 
 public class Main {
 
-    private static String msg = "Hello qa.guru!";
+    private static String msg = "Hello qa.guru!!!!!!";
     
     public static void main(String... args) {
         System.out.println(msg);
